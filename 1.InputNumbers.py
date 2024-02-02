bacd895d149378ea4d9c34ab8ee1cc1d6d6cd9c2
Komal-pixel-C <143890381+Komal-pixel-C@users.noreply.github.com>
2024-02-02 13:49:38 +0530

    Add files via upload
    Basic Data Types and Input Operations

---
+++ 1.InputNumbers.py
@@ -0,0 +1,29 @@
+"""
+Input() -> Result always STR
+int(input()) -> int
+float(input()) -> float
+
+"""
+# Sum of number
+
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+
+print("Sum of 2 number is :",a+b)
+
+
+# WAP input side of square and print its area
+ 
+length = float(input("Enter the side of square:"))
+
+print("Are of Square:",length*length)
+
+# WAP to input 2 numbers - a and b . Print  true if a is greate or equal to b else False
+
+"""
+if (a >= b):
+    print("True")
+else:
+    print("False")
+"""
+print(a>=b)
